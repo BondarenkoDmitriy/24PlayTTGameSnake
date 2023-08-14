cd5c0082a4c617bfb4f6faffe0577788e25df8c9
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-08-14 18:30:13 +0300

    added moving control

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
+const width = 640;
+const height = 480;
+
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.setSize( width, height );
 app.appendChild( renderer.domElement );
 
 const controls = new OrbitControls(camera, app);
@@ -30,13 +36,30 @@ class Snake {
   lastPosition: [number, number][] = [];
   direction: [number, number] = [0, -1];
   head: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>
+  children: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>[] = []
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
     this.head = genCube(x, y);
+
+    for (let i = 1; i < 5; i++) {
+      this.children.push(genCube(0, -i));
+      this.lastPosition.push([0, -i])
+    }
+  }
+
+  moveChildren() {
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].position.set(
+        this.lastPosition[this.lastPosition.length - 1 - i][0],
+        this.lastPosition[this.lastPosition.length - 1 - i][1],
+        0
+      )
+    }
   }
 
   move() {
+    this.lastPosition.push([this.x, this.y])
     this.x += this.direction[0];
     this.y += this.direction[1];
     this.head.position.set(this.x, this.y, 0);
@@ -59,14 +82,43 @@ camera.position.z = 10;
 
 function update() {
   snake.update();
-  setTimeout(update, 500)
+  setTimeout(update, 250)
 }
 
 update();
 
 function animation() {
   requestAnimationFrame( animation );
+  controls.update();
   renderer.render( scene, camera );
 }
 
 animation();
+
+window.onkeydown = e => {
+  switch(e.key) {
+    case "ArrowUp": 
+      if (snake.direction[1] === 0) {
+        snake.direction = [0, 1];
+      }
+      break;
+    case "ArrowDown":
+      if (snake.direction[1] === 0) {
+        snake.direction = [0, -1];
+      }
+      break;
+    case "ArrowLeft": 
+      if (snake.direction[0] === 0) {
+        snake.direction = [-1, 0];
+      }
+      break;
+    case "ArrowRight": 
+    if (snake.direction[0] === 0) {
+      snake.direction = [1, 0];
+    }
+      break;
+
+    default: 
+      break;
+  }
+}
