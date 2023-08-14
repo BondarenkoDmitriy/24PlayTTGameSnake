3ef36b4f60a3f55d6d6976161547f073e1026a6d
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-08-14 18:05:23 +0300

    the snake is moving

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
@@ -10,13 +11,58 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 app.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const controls = new OrbitControls(camera, app);
 
-scene.add( cube )
+function genCube(x: number, y: number) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  cube.position.set(x, y, 0);
+  scene.add( cube );
+  return cube;
+}
 
-camera.position.z = 5;
+// genCube(0, 0);
+
+class Snake {
+  x: number; 
+  y: number; 
+  lastPosition: [number, number][] = [];
+  direction: [number, number] = [0, -1];
+  head: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.head = genCube(x, y);
+  }
+
+  move() {
+    this.x += this.direction[0];
+    this.y += this.direction[1];
+    this.head.position.set(this.x, this.y, 0);
+  }
+
+  update() {
+    this.move();
+  }
+}
+
+let snake: Snake;
+
+function init() {
+  snake = new Snake(0, 0);
+}
+
+init();
+
+camera.position.z = 10;
+
+function update() {
+  snake.update();
+  setTimeout(update, 500)
+}
+
+update();
 
 function animation() {
   requestAnimationFrame( animation );
