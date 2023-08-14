ea10d1b84d8dd738d2a0699fcad2ba583a533972
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-08-14 19:51:15 +0300

    added shadow

--- src/main.ts
+++ src/main.ts
@@ -22,22 +22,20 @@ const controls = new OrbitControls(camera, app);
 
 function genCube(x: number, y: number) {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
   const cube = new THREE.Mesh( geometry, material );
   cube.position.set(x, y, 0);
   scene.add( cube );
   return cube;
 }
 
-// genCube(0, 0);
-
 class Snake {
   x: number; 
   y: number; 
   lastPosition: [number, number][] = [];
   direction: [number, number] = [0, -1];
-  head: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>
-  children: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>[] = []
+  head: THREE.Mesh<THREE.BoxGeometry, THREE.MeshStandardMaterial>
+  children: THREE.Mesh<THREE.BoxGeometry, THREE.MeshStandardMaterial>[] = []
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
@@ -108,10 +106,10 @@ let snake: Snake;
 class Apple {
   x: number = 3;
   y: number = 0;
-  apple: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>
+  apple: THREE.Mesh<THREE.BoxGeometry, THREE.MeshStandardMaterial>
   constructor(x: number, y: number) {
     this.apple = genCube(x, y);
-    this.apple.material = new THREE.MeshBasicMaterial({color: 0xff0000})
+    this.apple.material = new THREE.MeshStandardMaterial({color: 0xff0000})
     this.x = x
     this.y = y
   }
@@ -136,6 +134,26 @@ let apple: Apple;
 
 function init() {
   scene.clear()
+
+  const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+  scene.add( ambient );
+
+  const mainLight = new THREE.PointLight(0xe67710, 100, 500);
+  mainLight.position.set(-2, 0, 10);
+  scene.add( mainLight );
+
+  const rimLight = new THREE.PointLight(0x7f5adb, 100, 500);
+  rimLight.position.set(4, 0, -5);
+  scene.add( rimLight );
+
+  // const sphereSize = 1;
+  // const pointLightHelper = new THREE.PointLightHelper( mainLight, sphereSize );
+  // scene.add( pointLightHelper );
+
+  // const pointLightOtherHelper = new THREE.PointLightHelper( rimLight, sphereSize );
+  // scene.add( pointLightOtherHelper );
+
+  camera.position.set(0, 0, 10);
   points = 0;
   snake = new Snake(0, 0);
   apple = Apple.genRandomApple()
@@ -154,9 +172,6 @@ update();
 
 function animate() {
 	requestAnimationFrame( animate );
-
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
 
   controls.update();
 
@@ -186,6 +201,9 @@ window.onkeydown = e => {
     if (snake.direction[0] === 0) {
       snake.direction = [1, 0];
     }
+      break;
+    case "0": 
+      camera.position.set(0, 0, 10);
       break;
 
     default: 
